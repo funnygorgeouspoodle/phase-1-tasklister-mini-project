f6e126a517c07e5be4a6738f044c81f1992a96d2
Ashiq Legi <ashiqlegi@gmail.com>
2022-07-03 20:37:35 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    dataStorage(e.target.new_task_description.value)
+    form.reset()
+  
+  })
 });
+function dataStorage(toDoItem) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${toDoItem}`
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
